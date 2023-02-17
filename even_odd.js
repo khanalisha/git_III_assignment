b63335af854d3d6ff17c20a86ad73f5dc4e11efd
khanalisha <123863034+khanalisha@users.noreply.github.com>
2023-02-18 01:04:48 +0530

    Update even_odd.js
    I made changes so it  is more readable.

--- even_odd.js
+++ even_odd.js
@@ -1,11 +1,4 @@
 let num=13;
-<<<<<<< HEAD
+
 (num%2==0)?console.log("yes"):console.log("no");
-=======
-if(num%2==0){
-console.log("Even");
-}
-else {
-console.log("Odd");
-}
->>>>>>> ae2372e3c08cfeb5b38ba11945b8bde960c0c8a0
+
